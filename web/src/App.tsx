0c933533a4e11067b5fde0e6485e822eb90aab55
jmamadeu <jmamadeu2000@gmail.com>
2020-08-04 15:43:31 +0100

    :sparkles: feat: Settup Routes

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
+import Routes from './routes';
+
 import './assets/styles/global.css';
 
-import Landing from './pages/Landing';
-
 function App() {
-  return <Landing />;
+  return <Routes />;
 }
 
 export default App;
---
+++ web/src/routes.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Landing from './pages/Landing';
+import TeacherForm from './pages/TeacherForm';
+import TeacherList from './pages/TeacherList';
+
+const Routes: React.FC = () => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path='/' exact component={Landing} />
+        <Route path='/study' component={TeacherList} />
+        <Route path='/give-classes' component={TeacherForm} />
+      </Switch>
+    </BrowserRouter>
+  );
+};
+
+export default Routes;
